683b560952d23e69955a1256595619579fc8263d
gisman <gisman@gmail.com>
2025-08-22 01:06:14 +0900

    setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="korean-geocoder",
+    name="gimi9-geocoder",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="gisman",
+    author_email="gisman@gmail.com",
     description="A Python library for geocoding Korean addresses with integration for pandas.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/korean-geocoder",
+    url="https://github.com/gisman/gimi9_geocoder",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -17,12 +17,8 @@ setup(
     ],
     python_requires=">=3.6",
     install_requires=[
-        "fastapi",
-        "uvicorn",
-        "pandas",
-        "pyproj",
-        "pydantic",
         "httpx",
+        "pandas",
     ],
     extras_require={
         "dev": [
@@ -30,11 +26,6 @@ setup(
             "pytest-cov",
             "black",
             "flake8",
-        ],
-    },
-    entry_points={
-        "console_scripts": [
-            "gimi9-geocoder=gimi9_geocoder.client:main",
         ],
     },
 )
